a17f647ee1798834a6e2d85c15b32c09cece4076
naoki hirata <naoki@magic3.org>
2022-01-05 17:01:18 +0900

    スクリプトファイル名修正。

--- test/build_env.sh
+++ test/build_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/czbone/oneliner-lemp-on-docker/master/script/build_env.sh | bash /dev/stdin -test
+curl https://raw.githubusercontent.com/czbone/develop_servezero/master/script/build_env.sh | bash /dev/stdin -test
